a8112d6f84af4cab6895e17363aa0d77bd36d2b1
Vighnesh Pawar <vighneshpawar004@gmail.com>
2025-07-20 11:36:02 +0530

    add navbar

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -3,10 +3,10 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main >
+    < >
       <h1> Welcome to pizza shop</h1>
       <Button >Button</Button>
-    </main>
+    </>
 
   );
 }

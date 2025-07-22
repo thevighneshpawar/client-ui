bdefee5998c68262bb61064e5cff1894e8e284cb
Vighnesh Pawar <vighneshpawar004@gmail.com>
2025-07-22 11:54:40 +0530

    add attributes radio group

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,15 +1,51 @@
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Image from "next/image";
+import ProductCard, { Product } from "./components/ProductCard";
 
+const products: Product[] = [
+  {
+    id: "1",
+    name: "Margherita Pizza",
+    description: "Classic pizza with fresh tomatoes, mozzarella cheese, and basil.",
+    image: "/pizza-main.png",
+    price: 200,
+  },
+  {
+    id: "2",
+    name: "main Pizza",
+    description: "Classic pizza with fresh tomatoes, mozzarella cheese, and basil.",
+    image: "/pizza-main.png",
+    price: 200,
+  },
+  {
+    id: "3",
+    name: "main Pizza",
+    description: "Classic pizza with fresh tomatoes, mozzarella cheese, and basil.",
+    image: "/pizza-main.png",
+    price: 200,
+  },
+  {
+    id: "4",
+    name: "main Pizza",
+    description: "Classic pizza with fresh tomatoes, mozzarella cheese, and basil.",
+    image: "/pizza-main.png",
+    price: 200,
+  },
+  {
+    id: "5",
+    name: "main Pizza",
+    description: "Classic pizza with fresh tomatoes, mozzarella cheese, and basil.",
+    image: "/pizza-main.png",
+    price: 200,
+  }
+]
 
 export default function Home() {
   return (
     <>
-
       <section className="bg-white ">
         <div className="container px-12 mx-auto flex items-center justify-between py-24">
-
           <div>
             <h1 className="text-7xl font-black font-sans ">
               Super Delicious Pizza in <br />
@@ -22,18 +58,14 @@ export default function Home() {
               Get your pizza now
             </Button>
           </div>
-
-
           <div>
             <Image alt="pizza-main" src={'/pizza-main.png'} width={400} height={400} />
           </div>
-
         </div>
       </section>
-
       <section>
         <div className="container px-12 mx-auto py-12">
-          <Tabs defaultValue="pizza" className="w-[400px]">
+          <Tabs defaultValue="pizza" className="w-full">
             <TabsList>
               <TabsTrigger value="pizza" className="text-md">
                 Pizza
@@ -42,12 +74,19 @@ export default function Home() {
                 Beverages
               </TabsTrigger>
             </TabsList>
-            <TabsContent value="pizza">Pizza list</TabsContent>
+            <TabsContent value="pizza">
+              <div className="grid grid-cols-4 gap-6 mt-6">
+                {
+                  products.map((product: Product) => (
+                    <ProductCard key={product.id} product={product} />
+                  ))
+                }
+              </div>
+            </TabsContent>
             <TabsContent value="beverages">Beverages list </TabsContent>
           </Tabs>
         </div>
       </section>
     </>
-
   );
 }

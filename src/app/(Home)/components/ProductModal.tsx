2213d3646833b230ccfb15a6ab068b5582f7a17f
Vighnesh Pawar <vighneshpawar004@gmail.com>
2025-07-25 11:07:00 +0530

    add default priceconfig

--- src/app/(Home)/components/ProductModal.tsx
+++ src/app/(Home)/components/ProductModal.tsx
@@ -22,7 +22,17 @@ type ChosenConfig = {
 };
 const ProductModal = ({ product }: { product: Product }) => {
   const dispatch = useAppDispatch();
-  const [chosenConfig, setChosenConfig] = useState<ChosenConfig>();
+  const defaultConfiguration = Object.entries(
+    product.category.priceConfiguration
+  )
+    .map(([key, value]) => {
+      return { [key]: value.availableOptions[0] };
+    })
+    .reduce((acc, curr) => ({ ...acc, ...curr }), {});
+
+  const [chosenConfig, setChosenConfig] = useState<ChosenConfig>(
+    defaultConfiguration as unknown as ChosenConfig
+  );
   const [selectedToppings, setSelectedToppings] = React.useState<Topping[]>([]);
 
   const handleCheckBoxCheck = (topping: Topping) => {
